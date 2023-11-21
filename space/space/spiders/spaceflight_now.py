1c0da5d1ddc49b01d8ad66b282df1bd618de169b
poli <paolo.deidda@usi.ch>
2023-11-21 21:55:14 +0100

    hate for scrapy

--- space/space/spiders/spaceflight_now.py
+++ space/space/spiders/spaceflight_now.py
@@ -1,5 +1,6 @@
 import scrapy
 
+
 class TestSpider(scrapy.Spider):
     name = "spaceflight_now"
     start_urls = ["https://spaceflightnow.com/category/news-archive/"]
@@ -8,17 +9,34 @@ class TestSpider(scrapy.Spider):
         next_page_link = response.css('a.next::attr(href)').get()
 
         for article in response.css('article.mh-posts-list-item'):
+            title = article.css('div header h3 a::text').get().strip()
             article_link = article.css('div header h3 a::attr(href)').get()
-            yield response.follow(article_link, callback=self.parse_article)
+            date = article.css('div header div span a::text').get()
+            description = article.css('div div div p::text').get()
+
+            yield response.follow(
+                article_link,
+                callback=self.parse_article,
+                meta={'title': title, 'link': article_link, 'date': date, 'description': description}
+            )
 
         if next_page_link is not None:
             yield response.follow(next_page_link, callback=self.parse)
 
     def parse_article(self, response):
+        title = response.meta['title']
+        link = response.meta['link']
+        date = response.meta['date']
+        description = response.meta['description']
+
+        body = ""
+        for p in response.css('p::text'):
+            body += p.get().strip() + " "
+
         yield {
-            'title': response.css('div header h1::text').get().strip(),
-            'link': response.url,
-            'date': response.css('div header div span a::text').get(),
-            'description': response.css('div div div p::text').get(),
-            'body': ' '.join(response.css('article div.entry-content p::text').getall())
+            'title': title,
+            'link': link,
+            'date': date,
+            'description': description,
+            'body': body
         }

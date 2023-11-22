7ade245d47e933517b3b606a866b191c4286f5dc
poli <paolo.deidda@usi.ch>
2023-11-22 10:53:41 +0100

    removed some deprecated files

--- space/space/spiders/nasa.py
+++ space/space/spiders/nasa.py
@@ -1,5 +1,9 @@
 import time
 import scrapy
+import pyterrier as pt
+if not pt.started():
+  pt.init()
+import pandas as pd
 
 class Test_spider(scrapy.Spider):
 
@@ -12,23 +16,22 @@ class Test_spider(scrapy.Spider):
     def parse(self, response):    # Funcion called every crawled web page. The response parameter will contain the web site response.
         next_pages = []
 
+        i = 0
         for mission in response.xpath("//div[@class='hds-content-item hds-content-item-card hds-search-result mission-terms-result-container']"):
             yield {
+                    'missionNumber': i,
                     'mission_name': mission.xpath(".//h4[@class='hds-content-item-heading, color-carbon-black, heading-20, mission-terms-result-title']/text()"),
                     'text': mission.xpath(".//div[@class='hds-content-item-excerpt, mission-terms-result-excerpt']/text()")
             }
+            i = i + 1
 
-        # for mission in response.xpath("//div[@class='grid-item highlight mission']"):# For each mission element in the current page...
-        #     # print(mission)
-        #     # name = mission.xpath(".//h3[@class='heading']/text()").get()
-        #
-        #     link = mission.xpath(".//a[contains(concat( ' ', @class, ' ' ), concat( ' ', 'card', ' ' )) and contains(concat( ' ', @class, ' ' ), concat( ' ', 'highlight', ' ' )) and contains(concat( ' ', @class, ' ' ), concat( ' ', 'mission', ' ' ))]/@href").get()
-        #     if link is not None and link.startswith("/"):
-        #         next_pages.append("https://www.nasa.gov" + link)
-        #     # yield {'mission_name': name
-        #     #        , 'mission_link' : link
-        #     #        }
-        #     time.sleep(1)
-        # for page in next_pages:
-        #     yield response.follow(page, callback=self.parse)
-        #     # break
+
+
+        docs_df = pd.DataFrame[[
+            'mission_name',
+            'text'
+        ]]
+
+
+
+

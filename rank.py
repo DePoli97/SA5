6454f1b2ddf47881f6f78c3d7ed22bc202bfe97e
poli <paolo.deidda@usi.ch>
2023-12-04 23:57:08 +0100

    results_wiki.json added to the query reuslts?

--- rank.py
+++ rank.py
@@ -11,26 +11,27 @@ def prepare_df():
     df_esa = pd.read_json("./space/results_esa.json")
     ids = []
     texts = []
-    df_esa.rename(columns={'mission_name' : 'title', 'mission_description' : 'description', 'mission_page' : 'link'},inplace=True)
+    df_esa.rename(columns={'mission_name': 'title', 'mission_description': 'description', 'mission_page': 'link'},
+                  inplace=True)
     for i in range(len(df_esa)):
         ids.append(f"d{i}")
-    for i in range(df_esa.shape[0]):
         tmp = df_esa.iloc[i, :]
-        texts.append(tmp['title'] + " " + tmp['description']  + " " + tmp['link'])
+        texts.append(tmp['title'] + " " + tmp['description'] + " " + tmp['link'])
 
     df_s_now = pd.read_json("./space/results_spaceflight_no3.json")
-    for i in range(len(df_esa),len(df_s_now)+len(df_esa)):
+    for i in range(len(df_esa), len(df_s_now) + len(df_esa)):
         ids.append(f"d{i}")
+        tmp = df_s_now.iloc[i - len(df_esa)]
+        texts.append((tmp['title'] + " " + tmp['description'] + " " + tmp['body'][:-34] + " " +
+                       tmp['date'].strftime("%d-%B-%Y")).lower())
 
-    for i in range(df_s_now.shape[0]):
-        tmp = df_s_now.iloc[i, :]
-        texts.append((tmp['title'] + " " + tmp['description'] + " " + tmp['body'][:-34] + " " + tmp['date'].strftime("%d-%B-%Y")).lower())
+    df_wiki = pd.read_json("./space/results_wiki.json")
+    for i in range(len(df_s_now) + len(df_esa), len(df_s_now) + len(df_esa) + len(df_wiki)):
+        ids.append(f"d{i}")
+        tmp = df_wiki.iloc[i - (len(df_s_now) + len(df_esa))]
+        texts.append(tmp['Date'] + " " + tmp['Event'] + " " + tmp['Country'] + " " + tmp['Mission name'])
 
-    # df_wiki = pd.read_json("./space/result_wiki.json")
-    # for i in range(df_s_now.shape[0]):
-    #     print(df_s_now.iloc[i, :])
-    #     # texts.append((tmp['title'] + " " + tmp['description'] + " " + tmp['body'][:-34] + " " + tmp['date'].strftime("%d-%B-%Y")).lower())
-
+    # Verify lengths before creating the DataFrame
 
     df_all['docno'] = ids
     df_all['text'] = texts
@@ -44,26 +45,35 @@ def createIndex():
     index = pt.IndexFactory.of(index_ref)
 
 
-def query(query :str):
-    br = pt.BatchRetrieve(index, wmodel="BM25") #Query with a single word
-    queries = pd.DataFrame([["q1", str(query).lower()]], columns=["qid", "query"])
-    results = br.transform(queries)
-    docs_ids = []
-    # print(results.shape[0])
-    for i in range(results.shape[0]):
-        tmp = results.iloc[i, :]
-        docs_ids.append(tmp['docid'])
-        # if i >= 100:
-        #     break
-    docs_to_return = []
-    for id in docs_ids:
-        # print("id",id)
-        if (id > len(df_esa)):
-            doc = df_s_now.iloc[id-len(df_esa)]
-        else:
-            doc = df_esa.iloc[id]
-        docs_to_return.append({'title' : doc['title'], 'description' : doc['description'], 'link' : doc['link'] })
-    return docs_to_return
+def query(query: str):
+    try:
+        br = pt.BatchRetrieve(index, wmodel="BM25")
+        queries = pd.DataFrame([["q1", str(query).lower()]], columns=["qid", "query"])
+        results = br.transform(queries)
+        docs_ids = []
+
+        for i in range(results.shape[0]):
+            tmp = results.iloc[i, :]
+            docs_ids.append(tmp['docid'])
+
+        print(f"docs_ids: {docs_ids}")
+
+        docs_to_return = []
+        for doc_id in docs_ids:
+            if doc_id >= len(df_esa):
+                if doc_id - len(df_esa) >= len(df_s_now):
+                    print(f"Warning: Document ID {doc_id} is out-of-bounds for df_s_now.")
+                    continue
+                doc = df_s_now.iloc[doc_id - len(df_esa)]
+            else:
+                doc = df_esa.iloc[doc_id]
+            docs_to_return.append({'title': doc['title'], 'description': doc['description'], 'link': doc['link']})
+
+        return docs_to_return
+    except Exception as e:
+        print(f"Error in query processing: {e}")
+        raise
+
 
 def init():
     prepare_df()
